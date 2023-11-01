120f303f25bb3f75c3df8a9b771bb99705b7f4b3
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-11-01 13:04:47 -0400

    Initial Commit

---
+++ CueCard/CueCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CueCardApp.swift
+//  CueCard
+//
+//  Created by Quin Taylor on 2023-11-01.
+//
+
+import SwiftUI
+
+@main
+struct CueCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
