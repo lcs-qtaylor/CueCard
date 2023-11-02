c264ea6b865b805750d3fedf916104f4a57914d2
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-11-02 12:05:36 -0400

    Made a new View cardView that contains the the code for the cueCard so we have less to worry about on one veiw (not eliminating repetitive code but organizing things)

---
+++ CueCard/Helper/cardView.swift
@@ -0,0 +1,18 @@
+//
+//  cardView.swift
+//  CueCard
+//
+//  Created by Quin Taylor on 2023-11-02.
+//
+
+import SwiftUI
+
+struct cardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    cardView()
+}
