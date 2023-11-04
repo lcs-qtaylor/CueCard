c0f0eec94be02d555ea61eff1f1dc3a7eb258a6b
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-11-04 13:17:48 -0400

    Created addCardView this is what you will se when you click the add card button

---
+++ CueCard/views/addCardView.swift
@@ -0,0 +1,18 @@
+//
+//  addCardView.swift
+//  CueCard
+//
+//  Created by Quin Taylor on 2023-11-04.
+//
+
+import SwiftUI
+
+struct addCardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    addCardView()
+}
