5f201391920f05ba4938273787fe651445962850
Quinlan Taylor <qtaylor@lcs.on.ca>
2023-11-06 17:33:03 -0500

    got the my cards list working so I can show that the add card works

---
+++ CueCard/views/myCardsView.swift
@@ -0,0 +1,18 @@
+//
+//  myCardsView.swift
+//  CueCard
+//
+//  Created by Quin Taylor on 2023-11-06.
+//
+
+import SwiftUI
+
+struct myCardsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    myCardsView()
+}
